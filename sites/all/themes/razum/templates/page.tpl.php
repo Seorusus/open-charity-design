711128a881669ce04174e2c9e61450facc37cd41
Oleg Kreminsky <seorusus@gmail.com>
2018-01-26 14:46:30 +0000

    Created theme -4

---
+++ sites/all/themes/razum/templates/page.tpl.php
@@ -0,0 +1,11 @@
+<div id="page-wrapper" class="page-wrapper">
+  <div class="region-header">
+    <div id="logotype">
+      <?php if (!empty($logo)): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
