206538cff605d181daf89548c9cc0b8745947466
Oleg Kreminsky <seorusus@gmail.com>
2018-02-03 17:30:57 +0000

    Reloaded responsive_navigation

--- sites/all/modules/responsive_navigation/responsive_navigation.js
+++ sites/all/modules/responsive_navigation/responsive_navigation.js
@@ -9,7 +9,7 @@
     attach: function (context, settings) {
       settings.responsive_navigation = settings.responsive_navigation || {};
       Drupal.responsive_navigation.blocks = Drupal.responsive_navigation.blocks || {};
-      if (!$("#nav").size() || settings.responsive_navigation.total == 0) return; // end if no #nav is found or no blocks enabled.
+      if (!$("#nav").length || settings.responsive_navigation.total == 0) return; // end if no #nav is found or no blocks enabled.
       $.each(settings.responsive_navigation.blocks, function(key, value) {
         /* Method for handling multiple navs in the future */
         // key = key.split('_').join('-'); // convert _ to -
@@ -20,10 +20,10 @@
           label: value['responsive_navigation_label'], // String: Label for the navigation toggle
           insert: value['responsive_navigation_insert'], // String: Insert the toggle before or after the navigation
           openPos: value['responsive_navigation_openpos'], // String: Position of the opened nav, relative or static
-          jsClass: value['responsive_navigation_jsclass'], // String: 'JS enabled' class which is added to <html> el
+          jsClass: value['responsive_navigation_jsclass'] // String: 'JS enabled' class which is added to <html> el
         });
       });
-    },
+    }
   };
 
 }(jQuery));
