a440f54b1ee8f81972bb0ba5d87a9b73e2083ae5
Oleg Kreminsky <seorusus@gmail.com>
2018-02-06 10:02:50 +0000

    Added id mission

--- sites/all/themes/razum/templates/page--front.tpl.php
+++ sites/all/themes/razum/templates/page--front.tpl.php
@@ -43,22 +43,13 @@
         <div id="messages" class="clear clearfix"><?php print $messages; ?></div>
       <?php endif; ?>
 
-<!--      --><?php //print render($title_prefix); ?>
-<!--      --><?php //if ($title): ?>
-<!--        <div class="h1-title"><h1>--><?php //print $title; ?><!--</h1></div>-->
-<!--      --><?php //endif; ?>
-<!--      --><?php //print render($title_suffix); ?>
-
-<!--      --><?php //if ($tabs = render($tabs)): ?>
-<!--        <div class="tabs">--><?php //print $tabs; ?><!--</div>-->
-<!--      --><?php //endif; ?>
-
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
 
-
+      <div id="mission">
       <?php echo render($page['content']); ?>
+      </div>
 <!-- Region Members -->
       <div class="members">
         <?php echo render($page['members']); ?>
